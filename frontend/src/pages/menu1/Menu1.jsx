aca5868b5aaf880a3faca64b33ae42cb91f9a147
Syahrul Safarila <sysafarila.official@gmail.com>
2020-10-03 07:30:34 +0700

    Update Menu1.jsx

--- frontend/src/pages/menu1/Menu1.jsx
+++ frontend/src/pages/menu1/Menu1.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import config from "../../config.json";
 import AuthMiddleware from "../../middleware/AuthMiddleware";
 
 const Menu1 = () => {
+  const [name, setName] = useState("loading...");
   useEffect(() => {
     const getUser = async () => {
       await axios
         .get(`${config.apiUrl}/user`)
         .then((res) => {
           console.log(res);
+          setName(res.data.name);
         })
         .catch((err) => {
           console.log(err);
@@ -21,6 +23,7 @@ const Menu1 = () => {
   return (
     <AuthMiddleware>
       <h1>Menu 1</h1>
+      <h3>{name}</h3>
     </AuthMiddleware>
   );
 };
