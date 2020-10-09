0d55101b5b4591e2b7c1e6c622803378b022007e
Syahrul Safarila <sysafarila.official@gmail.com>
2020-10-09 15:09:13 +0700

    Update Loading

--- frontend/src/pages/loading/Loading.jsx
+++ frontend/src/pages/loading/Loading.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Loading.css";
+import LoadingAnimate from "./Loading.svg";
 
 const Loading = (props) => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="lobster lg:text-4xl lobster text-3xl text-gray-700">
-        {props.message ? props.message : "Loading..."}
+        {/* {props.message ? props.message : "Loading..."} */}
+        <img src={LoadingAnimate} alt="Loading..." />
       </div>
     </div>
   );
